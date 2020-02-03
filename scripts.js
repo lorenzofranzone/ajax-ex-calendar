b96334e8fcaeecd400871d5e1ffdf68154fae04f
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-03 14:33:27 +0100

    Jan List 1-31

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
 
   // Code
-  
+  // January
+  var jan = moment('2018-01');
+  console.log(jan);
+
+  // Days Number in month
+  var daysInJan = jan.daysInMonth();
+  console.log(daysInJan);
+
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+    method: "GET",
+    // SUCCESS
+    success: function (data) {
+      var source = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+      for (var i = 1; i <= daysInJan; i++) {
+        var context = { body: i + ' Gennaio' };
+        var html = template(context);
+        $('ul').append(html);
+      }
+    },
+    // ERROR
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+
+
 
 
   //////////////////////////////////////////////////
